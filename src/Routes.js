622bcc29629159c9b03434515ade3ca107bd3bb4
Tatsapat Saerejittima <sabaiprimo@gmail.com>
2021-04-22 14:31:43 +0300

    Add article views

--- src/Routes.js
+++ src/Routes.js
@@ -8,7 +8,12 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import WithLayout from 'WithLayout';
 import { Main as MainLayout, Minimal as MinimalLayout } from './layouts';
 
-import { IndexView, BrowseRecipe as BrowseRecipeViewView } from './views';
+import {
+  IndexView,
+  BrowseRecipe as BrowseRecipeViewView,
+  BrowseArticles as BrowseArticlesView,
+  SingleArticle as SingleArticleView,
+} from './views';
 
 const Routes = () => {
   return (
@@ -31,6 +36,28 @@ const Routes = () => {
           <WithLayout
             {...matchProps}
             component={BrowseRecipeViewView}
+            layout={MainLayout}
+          />
+        )}
+      />
+      <Route
+        exact
+        path='/blog-articles'
+        render={(matchProps) => (
+          <WithLayout
+            {...matchProps}
+            component={BrowseArticlesView}
+            layout={MainLayout}
+          />
+        )}
+      />
+      <Route
+        exact
+        path='/single-article'
+        render={(matchProps) => (
+          <WithLayout
+            {...matchProps}
+            component={SingleArticleView}
             layout={MainLayout}
           />
         )}
--- src/views/index.js
+++ src/views/index.js
@@ -1,2 +1,4 @@
 export { default as IndexView } from './IndexView';
 export { default as BrowseRecipe } from './BrowseRecipe';
+export { default as BrowseArticles } from './BrowseArticles';
+export { default as SingleArticle } from './SingleArticle';
