f102b27925e96b5f30ce99a2b2c78401facdf254
Tatsapat Saerejittima <sabaiprimo@gmail.com>
2021-04-26 23:07:38 +0300

    Connect api reset password

--- src/views/Account/components/General/General.js
+++ src/views/Account/components/General/General.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+
 import {
   useMediaQuery,
   Grid,
@@ -10,8 +11,58 @@ import {
   Divider,
 } from '@material-ui/core';
 import AvatarUpload from '../AvatarUpload/AvatarUpload.js';
-import { useSelector } from 'react-redux';
-import { userSelector } from '../../../../features/user/UserSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  userSelector,
+  setUserProfile,
+} from '../../../../features/user/UserSlice';
+import { gql, useMutation } from '@apollo/client';
+import validate from 'validate.js';
+
+const schema = {
+  firstName: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 20,
+    },
+  },
+  lastName: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 20,
+    },
+  },
+  displayName: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 20,
+    },
+  },
+};
+
+const UPDATE_USER_PROFILE = gql`
+  mutation updateUserProfile(
+    $userID: ID
+    $updateDisplayName: String
+    $updateFirstName: String
+    $updateLastName: String
+    $updatePictureProfile: String
+  ) {
+    modifyUser(
+      _id: $userID
+      displayName: $updateDisplayName
+      firstName: $updateFirstName
+      lastName: $updateLastName
+      pictureProfile: $updatePictureProfile
+    ) {
+      firstName
+      lastName
+      displayName
+      pictureProfile
+    }
+  }
+`;
+
 const useStyles = makeStyles((theme) => ({
   inputTitle: {
     fontWeight: 700,
@@ -22,9 +73,99 @@ const useStyles = makeStyles((theme) => ({
 const General = (props) => {
   const { className, ...rest } = props;
   const classes = useStyles();
-  const { firstName, lastName, displayName, pictureProfile } = useSelector(
-    userSelector
-  );
+  const {
+    _id,
+    firstName,
+    lastName,
+    displayName,
+    pictureProfile,
+    tempUploadProfile,
+  } = useSelector(userSelector);
+
+  const [formState, setFormState] = React.useState({
+    isValid: false,
+    values: {
+      firstName: firstName,
+      lastName: lastName,
+      displayName: displayName,
+      pictureProfile: pictureProfile,
+    },
+    touched: {},
+    errors: {},
+  });
+
+  const hasError = (field) =>
+    formState.touched[field] && formState.errors[field] ? true : false;
+
+  React.useEffect(() => {
+    const errors = validate(formState.values, schema);
+
+    setFormState((formState) => ({
+      ...formState,
+      isValid: errors ? false : true,
+      errors: errors || {},
+    }));
+  }, [formState.values]);
+  const [updateUserProfile, result] = useMutation(UPDATE_USER_PROFILE);
+
+  React.useEffect(() => {
+    if (result.data) {
+      console.log(result.data);
+    }
+  }, [result.data]);
+
+  const handleChange = (event) => {
+    event.persist();
+
+    setFormState((formState) => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [event.target.name]:
+          event.target.type === 'checkbox'
+            ? event.target.checked
+            : event.target.value,
+      },
+      touched: {
+        ...formState.touched,
+        [event.target.name]: true,
+      },
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (formState.isValid) {
+      const userID = _id;
+      const updateDisplayName = formState.values.displayName;
+      const updateFirstName = formState.values.firstName;
+      const updateLastName = formState.values.lastName;
+      const updatePictureProfile = tempUploadProfile;
+      console.log(userID);
+      console.log(updatePictureProfile);
+      updateUserProfile({
+        variables: {
+          userID,
+          updateDisplayName,
+          updateFirstName,
+          updateLastName,
+          updatePictureProfile,
+        },
+      });
+
+      // window.location.replace('/');
+    }
+
+    setFormState((formState) => ({
+      ...formState,
+      touched: {
+        ...formState.touched,
+        ...formState.errors,
+      },
+    }));
+  };
+
   const theme = useTheme();
   const isMd = useMediaQuery(theme.breakpoints.up('md'), {
     defaultMatches: true,
@@ -32,83 +173,101 @@ const General = (props) => {
 
   return (
     <div className={className} {...rest}>
-      <Grid container spacing={isMd ? 4 : 2}>
-        <Grid item xs={12}>
-          <Typography variant='h6' color='textPrimary'>
-            Basic Information
-          </Typography>
+      <form name='password-reset-form' method='post' onSubmit={handleSubmit}>
+        <Grid container spacing={isMd ? 4 : 2}>
+          <Grid item xs={12}>
+            <Typography variant='h6' color='textPrimary'>
+              Basic Information
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Divider />
+          </Grid>
+          <Grid item xs={12}>
+            <AvatarUpload />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Typography
+              variant='subtitle1'
+              color='textPrimary'
+              className={classes.inputTitle}
+            >
+              First name
+            </Typography>
+            <TextField
+              placeholder='Your first name'
+              variant='outlined'
+              size='medium'
+              name='firstName'
+              fullWidth
+              type='text'
+              onChange={handleChange}
+              value={formState.values.firstName || ''}
+              helperText={
+                hasError('firstName') ? formState.errors.firstName[0] : null
+              }
+              error={hasError('firstName')}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography
+              variant='subtitle1'
+              color='textPrimary'
+              className={classes.inputTitle}
+            >
+              Last name
+            </Typography>
+            <TextField
+              placeholder='Your last name'
+              variant='outlined'
+              size='medium'
+              name='lastName'
+              fullWidth
+              type='text'
+              onChange={handleChange}
+              value={formState.values.lastName || ''}
+              helperText={
+                hasError('lastName') ? formState.errors.lastName[0] : null
+              }
+              error={hasError('lastName')}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              variant='subtitle1'
+              color='textPrimary'
+              className={classes.inputTitle}
+            >
+              Display Name
+            </Typography>
+            <TextField
+              placeholder='Your display name'
+              variant='outlined'
+              size='medium'
+              name='displayName'
+              fullWidth
+              type='text'
+              onChange={handleChange}
+              value={formState.values.displayName || ''}
+              helperText={
+                hasError('displayName') ? formState.errors.displayName[0] : null
+              }
+              error={hasError('displayName')}
+            />
+          </Grid>
+          <Grid item container justify='flex-start' xs={12}>
+            <Button
+              variant='contained'
+              type='submit'
+              color='primary'
+              size='large'
+            >
+              save
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
-        <Grid item xs={12}>
-          <AvatarUpload />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography
-            variant='subtitle1'
-            color='textPrimary'
-            className={classes.inputTitle}
-          >
-            First name
-          </Typography>
-          <TextField
-            placeholder='Your first name'
-            variant='outlined'
-            size='medium'
-            name='firstName'
-            fullWidth
-            type='text'
-            value={firstName}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography
-            variant='subtitle1'
-            color='textPrimary'
-            className={classes.inputTitle}
-          >
-            Last name
-          </Typography>
-          <TextField
-            placeholder='Your last name'
-            variant='outlined'
-            size='medium'
-            name='lastName'
-            fullWidth
-            type='text'
-            value={lastName}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography
-            variant='subtitle1'
-            color='textPrimary'
-            className={classes.inputTitle}
-          >
-            Display Name
-          </Typography>
-          <TextField
-            placeholder='Your display name'
-            variant='outlined'
-            size='medium'
-            name='address'
-            fullWidth
-            type='text'
-            value={displayName}
-          />
-        </Grid>
-        <Grid item container justify='flex-start' xs={12}>
-          <Button
-            variant='contained'
-            type='submit'
-            color='primary'
-            size='large'
-          >
-            save
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
     </div>
   );
 };
--- src/views/Account/components/Security/Security.js
+++ src/views/Account/components/Security/Security.js
@@ -11,8 +11,52 @@ import {
   Button,
   Divider,
 } from '@material-ui/core';
+import validate from 'validate.js';
+import { gql, useMutation } from '@apollo/client';
+import {
+  userSelector,
+  setUserProfile,
+} from '../../../../features/user/UserSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
-const useStyles = makeStyles(theme => ({
+const schema = {
+  oldPassword: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      minimum: 8,
+    },
+  },
+  newPassword: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      minimum: 8,
+    },
+  },
+  oldPassword: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      minimum: 8,
+    },
+  },
+};
+
+const UPDATE_PASSWORD = gql`
+  mutation changeUserPassword(
+    $userID: ID
+    $oldPassword: String
+    $newPassword: String
+  ) {
+    changePassword(
+      _id: $userID
+      oldPassword: $oldPassword
+      newPassword: $newPassword
+    ) {
+      _id
+    }
+  }
+`;
+
+const useStyles = makeStyles((theme) => ({
   inputTitle: {
     fontWeight: 700,
     marginBottom: theme.spacing(1),
@@ -27,9 +71,92 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const Security = props => {
+const Security = (props) => {
   const { className, ...rest } = props;
+
   const classes = useStyles();
+  const { _id } = useSelector(userSelector);
+  const [formState, setFormState] = React.useState({
+    isValid: false,
+    values: {},
+    touched: {},
+    errors: {},
+  });
+  const [changeUserPassword, updatePasswordResult] = useMutation(
+    UPDATE_PASSWORD
+  );
+
+  const handleChange = (event) => {
+    event.persist();
+
+    setFormState((formState) => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [event.target.name]:
+          event.target.type === 'checkbox'
+            ? event.target.checked
+            : event.target.value,
+      },
+      touched: {
+        ...formState.touched,
+        [event.target.name]: true,
+      },
+    }));
+  };
+
+  React.useEffect(() => {
+    console.log(updatePasswordResult.data);
+    // alert('Change password Success');
+  }, [updatePasswordResult.data]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (formState.isValid) {
+      const oldPassword = formState.values.oldPassword;
+      const newPassword = formState.values.newPassword;
+      const userID = _id;
+
+      changeUserPassword({
+        variables: {
+          userID,
+          oldPassword,
+          newPassword,
+        },
+      })
+        .then(() => {
+          alert('Update password complete');
+          window.location.reload();
+        })
+        .catch((error) => {
+          alert(error);
+          window.location.reload();
+        });
+
+      setFormState({ isValid: false, values: {}, touched: {}, errors: {} });
+    }
+
+    setFormState((formState) => ({
+      ...formState,
+      touched: {
+        ...formState.touched,
+        ...formState.errors,
+      },
+    }));
+  };
+  React.useEffect(() => {
+    let errors = validate(formState.values, schema);
+    if (formState.values.repeatPassword !== formState.values.newPassword) {
+      errors = { ...errors, repeatPassword: ['Password does not match'] };
+    }
+    setFormState((formState) => ({
+      ...formState,
+      isValid: errors ? false : true,
+      errors: errors || {},
+    }));
+  }, [formState.values]);
+  const hasError = (field) =>
+    formState.touched[field] && formState.errors[field] ? true : false;
 
   const theme = useTheme();
   const isMd = useMediaQuery(theme.breakpoints.up('md'), {
@@ -38,118 +165,104 @@ const Security = props => {
 
   return (
     <div className={className} {...rest}>
-      <Grid container spacing={isMd ? 4 : 2}>
-        <Grid item xs={12}>
-          <div className={classes.titleCta}>
-            <Typography variant="h6" color="textPrimary">
-              Change Password
+      <form name='password-reset-form' method='post' onSubmit={handleSubmit}>
+        <Grid container spacing={isMd ? 4 : 2}>
+          <Grid item xs={12}>
+            <div className={classes.titleCta}>
+              <Typography variant='h6' color='textPrimary'>
+                Change Password
+              </Typography>
+              <Button variant='outlined' color='primary'>
+                Log out
+              </Button>
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <Divider />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              variant='subtitle1'
+              color='textPrimary'
+              className={classes.inputTitle}
+            >
+              Current password
             </Typography>
-            <Button variant="outlined" color="primary">
-              Log out
+            <TextField
+              placeholder='Old password'
+              variant='outlined'
+              size='medium'
+              name='oldPassword'
+              fullWidth
+              type='password'
+              onChange={handleChange}
+              helperText={
+                hasError('oldPassword') ? formState.errors.oldPassword[0] : null
+              }
+              error={hasError('oldPassword')}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              variant='subtitle1'
+              color='textPrimary'
+              className={classes.inputTitle}
+            >
+              New password
+            </Typography>
+            <TextField
+              placeholder='New password'
+              variant='outlined'
+              size='medium'
+              name='newPassword'
+              fullWidth
+              type='password'
+              onChange={handleChange}
+              helperText={
+                hasError('newPassword') ? formState.errors.newPassword[0] : null
+              }
+              error={hasError('newPassword')}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              variant='subtitle1'
+              color='textPrimary'
+              className={classes.inputTitle}
+            >
+              Repeat password
+            </Typography>
+            <TextField
+              placeholder='Repeat password'
+              variant='outlined'
+              size='medium'
+              name='repeatPassword'
+              fullWidth
+              type='password'
+              onChange={handleChange}
+              helperText={
+                hasError('repeatPassword')
+                  ? formState.errors.repeatPassword[0]
+                  : null
+              }
+              error={hasError('repeatPassword')}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Divider />
+          </Grid>
+          <Grid item container justify='flex-start' xs={12}>
+            <Button
+              variant='contained'
+              type='submit'
+              color='primary'
+              size='large'
+            >
+              save
             </Button>
-          </div>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography
-            variant="subtitle1"
-            color="textPrimary"
-            className={classes.inputTitle}
-          >
-            Current password
-          </Typography>
-          <TextField
-            placeholder="Old password"
-            variant="outlined"
-            size="medium"
-            name="fullname"
-            fullWidth
-            type="password"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography
-            variant="subtitle1"
-            color="textPrimary"
-            className={classes.inputTitle}
-          >
-            New password
-          </Typography>
-          <TextField
-            placeholder="New password"
-            variant="outlined"
-            size="medium"
-            name="fullname"
-            fullWidth
-            type="password"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography
-            variant="subtitle1"
-            color="textPrimary"
-            className={classes.inputTitle}
-          >
-            Repeat password
-          </Typography>
-          <TextField
-            placeholder="Repeat password"
-            variant="outlined"
-            size="medium"
-            name="fullname"
-            fullWidth
-            type="password"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Switch color="primary" defaultChecked />}
-            label={
-              <Typography
-                variant="subtitle1"
-                color="textPrimary"
-                className={classes.switchTitle}
-              >
-                Public Profile
-              </Typography>
-            }
-            labelPlacement="end"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Switch color="primary" />}
-            label={
-              <Typography
-                variant="subtitle1"
-                color="textPrimary"
-                className={classes.switchTitle}
-              >
-                Expose your email
-              </Typography>
-            }
-            labelPlacement="end"
-          />
-        </Grid>
-        <Grid item container justify="flex-start" xs={12}>
-          <Button
-            variant="contained"
-            type="submit"
-            color="primary"
-            size="large"
-          >
-            save
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
     </div>
   );
 };
